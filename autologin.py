81c8d7cec746a03cba0f0650aae0d1797213acf5
jmarshall-navent <jmarshall@navent.com>
2017-01-19 16:42:10 -0300

    Update autologin.py

--- autologin.py
+++ autologin.py
@@ -4,10 +4,12 @@ import os
 
 def getAccessTokenEmpresaAutologin( ):
   idEmpresa = os.environ['ID_EMPRESA']
-  portal = os.environ['PORTAL']
+  ambiente = os.envirot['AMBIENTE']
+  
+  ambientesUrl = {"bm-qa" :  "apiqa.navent.com" , "bm-sandbox" : "https://developers.bumeran.com" ,  "bm-lite" : "192.168.120.212:8080", "zj-sandbox" : "https://developers.zonajobs.com"}
   
   headers = {'Content-type' : 'application/json', 'Accept' : 'application/json'}
-  basepath = "https://developers." + portal + ".com/v0/"
+  basepath = ambientesUrl[ambeinte] + "/v0/"
   
   url = basepath + "application/oauth2/login?grant_type=client_credentials&client_id=api-developer&client_secret=secret"
   r = requests.post(url, headers = headers, verify = False)
