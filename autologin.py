fc52bb02fa26e18b9e41a18b94564b5d3062cf18
jmarshall-navent <jmarshall@navent.com>
2017-01-19 13:03:13 -0300

    probar loguear como app

---
+++ autologin.py
@@ -0,0 +1,10 @@
+import requests
+
+
+headers = {'Content-type' : 'application/json', 'Accept' : 'application/json'}
+
+endpoint = "https://developers.zonajobs.com/v0/application/oauth2/login"
+url = "https://developers.zonajobs.com/v0/application/oauth2/login?grant_type=client_credentials&client_id=api-developer&client_secret=secret"
+r = requests.post(url, headers = headers)
+
+print r
