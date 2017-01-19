2995e46ed85c351ece30bab22ad75e8bb7c76cc6
jmarshall-navent <jmarshall@navent.com>
2017-01-19 14:53:05 -0300

    Update autologin.py

--- autologin.py
+++ autologin.py
@@ -1,35 +1,36 @@
 import requests
 import json
 
-
-headers = {'Content-type' : 'application/json', 'Accept' : 'application/json'}
-
-endpoint = "https://developers.zonajobs.com/v0/application/oauth2/login"
-url = "https://developers.zonajobs.com/v0/application/oauth2/login?grant_type=client_credentials&client_id=api-developer&client_secret=secret"
-r = requests.post(url, headers = headers, verify = False)
-
-
-
-accessTokenApp = r.json()['access_token']
+def getAccessTokenEmpresaAutologin( idEmpresa , portal):
+  headers = {'Content-type' : 'application/json', 'Accept' : 'application/json'}
+  basepath = " https://developers." + portal + ".com/v0/"
+  
+  url = basepath + "application/oauth2/login?grant_type=client_credentials&client_id=api-developer&client_secret=secret"
+  r = requests.post(url, headers = headers, verify = False)
 
 
 
-endpointEmpresasToken = "https://developers.zonajobs.com/v0/application/empresas/token?access_token=" + accessTokenApp
-usuario = {'usuarioId': 543287} 
-
-r2 = requests.post(endpointEmpresasToken, headers = headers, verify = False, data = json.dumps(usuario))
+  accessTokenApp = r.json()['access_token']
 
 
 
-accesTokenAutologin = r2.json()['token']
+  endpointEmpresasToken = basepath + "application/empresas/token?access_token=" + accessTokenApp
+  usuario = {'usuarioId': idEmpresa} 
+
+  r2 = requests.post(endpointEmpresasToken, headers = headers, verify = False, data = json.dumps(usuario))
 
 
 
-endpointEmpresasAutologin = "https://developers.zonajobs.com/v0/application/empresas/autologin?grant_type=autologin&client_id=api-developer&client_secret=secret&token=" + accesTokenAutologin
+  accesTokenAutologin = r2.json()['token']
 
-r3 = requests.post(endpointEmpresasAutologin, headers = headers, verify = False)
 
-empresaToken = r3.json()['access_token']
-print '\n \n \n '
-print "acces_token para la empresa con id : "
-print empresaToken
+
+  endpointEmpresasAutologin = basepath + "application/empresas/autologin?grant_type=autologin&client_id=api-developer&client_secret=secret&token=" + accesTokenAutologin
+
+  r3 = requests.post(endpointEmpresasAutologin, headers = headers, verify = False)
+
+  empresaToken = r3.json()['access_token']
+  print '\n \n \n '
+  print "acces_token para la empresa con id : " + idEmpresa
+  print '\n '
+  print empresaToken
