83aa56bedb267f5df0907ef53dc5af41bc9336f8
jmarshall-navent <jmarshall@navent.com>
2017-01-19 13:05:53 -0300

    Update autologin.py

--- autologin.py
+++ autologin.py
@@ -5,6 +5,6 @@ headers = {'Content-type' : 'application/json', 'Accept' : 'application/json'}
 
 endpoint = "https://developers.zonajobs.com/v0/application/oauth2/login"
 url = "http://developers.zonajobs.com/v0/application/oauth2/login?grant_type=client_credentials&client_id=api-developer&client_secret=secret"
-r = requests.post(url, headers = headers)
+r = requests.post(url, headers = headers, verify = False)
 
 print r
