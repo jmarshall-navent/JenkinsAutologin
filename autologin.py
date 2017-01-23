49133e898c98d50ae5faae4a0acd5169bad1390f
jmarshall-navent <jmarshall@navent.com>
2017-01-23 11:29:46 -0300

    Update autologin.py

--- autologin.py
+++ autologin.py
@@ -9,7 +9,6 @@ def getAccessTokenEmpresaAutologin():
   
   print idUsuario
   print ambiente
-  print usuario
 
   ambientesUrl = {"bm-produccion" :  "https://api.bumeran.com" ,"zj-produccion" : "https://api.zonajobs.com.ar" , "bm-sandbox" : "https://developers.bumeran.com" ,  "bm-lite" : "http://192.168.120.212:8080", "zj-sandbox" : "https://developers.zonajobs.com"}
   
@@ -28,6 +27,7 @@ def getAccessTokenEmpresaAutologin():
 
 
   if(usuario == "Empresa"):
+      print usuario
     endpointToken = basepath + "application/empresas/token?access_token=" + accessTokenApp
   else:
     endpointToken = basepath + "application/postulantes/token?acces_token" + accessTokenApp
@@ -44,6 +44,7 @@ def getAccessTokenEmpresaAutologin():
   print accessTokenAutologin
 
   if(usuario == "Empresa"):
+      print usuario
     endpointAutologin = basepath + "application/empresas/autologin?grant_type=autologin&client_id=api-developer&client_secret=secret&token=" + accessTokenAutologin
   else:
     endpointAutologin = basepath + "application/postulantes/autologin?grant_type=autologin&client_id=api-developer&client_secret=secret&token=" + accessTokenAutologin
